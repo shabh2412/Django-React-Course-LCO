05b7d92304b4d774bc82b94bf35b4d2238b56cf4
Rishabh Panesar <rishabh.sagarika@gmail.com>
2021-06-24 14:06:35 +0530

    Created View set for animation objects

--- ecom/api/textractor/views.py
+++ ecom/api/textractor/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AnimationObjectSerializer
+from .models import AnimationObject
 # Create your views here.
+
+class AnimationObjectViewSet(viewsets.ModelViewSet):
+    # 
+    queryset = AnimationObject.objects.all().order_by('name')
+    serializer_class = AnimationObjectSerializer
+    
+    # Now you need to setup the urls
