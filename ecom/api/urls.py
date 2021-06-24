2380335660d282a2e9806adba8096da9ecf9ea12
Rishabh Panesar <rishabh.sagarika@gmail.com>
2021-06-24 14:09:01 +0530

    added route to handle textractor api requests

--- ecom/api/urls.py
+++ ecom/api/urls.py
@@ -5,5 +5,6 @@ from .views import home
 
 urlpatterns = [
     path('', home, name='api.home'),
-    path('category/', include('api.category.urls'))
+    path('category/', include('api.category.urls')),
+    path('textractor/', include('api.textractor.urls')),
 ]
